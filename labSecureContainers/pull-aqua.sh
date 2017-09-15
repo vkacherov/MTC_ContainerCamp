d3937de8dd3db892989c4297c2daa99162051929
root <root@aqualab.rtcphub0j4zeplaqxbrsumb5sf.bx.internal.cloudapp.net>
2017-09-15 22:36:57 +0000

    tweak-scripts

--- labSecureContainers/pull-aqua.sh
+++ labSecureContainers/pull-aqua.sh
@@ -1,13 +1,10 @@
-docker pull aquasec/server:2.5.2
-docker pull aquasec/gateway:2.5.2
-docker pull aquasec/scanner-cli:2.5.2
-docker pull aquasec/database:2.5.2
+wget -O- https://get.aquasec.com/$1/aquasec-server-2.5.3.tar.gz | gunzip -c > aquasec-server-2.5.3.tar
+wget -O- https://get.aquasec.com/$1/aquasec-gateway-2.5.3.tar.gz | gunzip -c > aquasec-gateway-2.5.3.tar
+wget -O- https://get.aquasec.com/$1/aquasec-database-2.5.3.tar.gz | gunzip -c > aquasec-database-2.5.3.tar
+wget -O- https://get.aquasec.com/$1/aquasec-scanner-cli-2.5.3.tar.gz | gunzip -c > aquasec-scanner-cli-2.5.3.tar
+wget -O- https://get.aquasec.com/$1/aquasec-agent-2.5.3.tar.gz | gunzip -c > aquasec-agent-2.5.3.tar
 
-docker pull ravitella/docker-springboot-recommendationservice:latest
-docker pull ravitella/docker-springboot-readinglistapplication:latest
-docker pull mysql/mysql-server:latest
-
-docker load -i /opt/lab-files/aquasec-scanner-cli.tar
-docker load -i /opt/lab-files/aquasec-gateway.tar
-docker load -i /opt/lab-files/aquasec-server.tar
-docker load -i /opt/lab-files/aquasec-database.tar
+docker load -i aquasec-server-2.5.3.tar
+docker load -i aquasec-gateway-2.5.3.tar
+docker load -i aquasec-database-2.5.3.tar
+docker load -i aquasec-scanner-cli-2.5.3.tar
